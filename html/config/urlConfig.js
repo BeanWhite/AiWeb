0bb33ebbce0e26fae4fb7fe934a423c425e58055
BeanWhite <1452355437@qq.com>
2020-03-25 12:24:51 +0800

    dfa

--- html/config/urlConfig.js
+++ html/config/urlConfig.js
@@ -10,13 +10,13 @@ var config = {
 	
 	
 	//imgPrintUrl:'http://localhost:81/xlrzpc/img/cacheImg',
-	// url:"http://localhost:88",
-	// wsURL:"ws://localhost:88",
+	url:"http://localhost:88",
+	wsURL:"ws://localhost:88",
 	// imgPrintUrl:'http://localhost:8848/AiWeb/img/cacheImg'
 	
 	// imgPrintUrl:'http://192.168.1.105:8848/AiWeb/img/cacheImg'	,//pdf图片url
-	url:"http://192.168.62.61:88",
-	wsURL:"ws://192.168.62.61:88",
+	// url:"http://192.168.62.61:88",
+	// wsURL:"ws://192.168.62.61:88",
 	// url:"http://192.168.1.101:88",
 	// wsURL:"ws://192.168.1.101:88",
 	//url:"http://192.168.1.105:88",
