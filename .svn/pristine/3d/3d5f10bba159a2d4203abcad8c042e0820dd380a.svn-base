4d4271d2b4df1d4079bcaa3f0902e3c19ba3ae88
BeanWhite <1452355437@qq.com>
2020-04-30 07:57:38 +0800

    dafds

---
+++ .svn/pristine/3d/3d5f10bba159a2d4203abcad8c042e0820dd380a.svn-base
@@ -0,0 +1,4 @@
+let USER_ID =JSON.parse(sessionStorage.getItem('logins')) ;
+if (USER_ID == null || USER_ID == undefined)
+	location.href = '../index.html'
+USER_ID = USER_ID.id
--- html/config/urlConfig.js
+++ html/config/urlConfig.js
@@ -1,19 +1,19 @@
 var config = {
-	// url:'http://122.112.225.219:88',
-	// wsURL:'ws://122.112.225.219:88', 
-	
-	
-	
-	imgPrintUrl:'D:/nginx-1.17.7/html/xlrzpc/img/cacheImg',//服务器使用
+	url:'http://122.112.225.219:88',
+	wsURL:'ws://122.112.225.219:88', 
+
+
+
+	imgPrintUrl: 'D:/nginx-1.17.7/html/xlrzpc/img/cacheImg', //服务器使用
 	//imgPrintUrl:'http://localhost:81/xlrzpc/img/cacheImg',
 	//imgPrintUrl:'D://mmse222.png'
-	
-	
+
+
 	//imgPrintUrl:'http://localhost:81/xlrzpc/img/cacheImg',
-	url:"http://localhost:88",
-	wsURL:"ws://localhost:88",
+	// url: "http://localhost:88",
+	// wsURL: "ws://localhost:88",
 	// imgPrintUrl:'http://localhost:8848/AiWeb/img/cacheImg'
-	
+
 	// imgPrintUrl:'http://192.168.1.105:8848/AiWeb/img/cacheImg'	,//pdf图片url
 	// url:"http://192.168.62.61:88",
 	// wsURL:"ws://192.168.62.61:88",
@@ -26,5 +26,4 @@ var config = {
 	//  */
 	// imgUrl_test:'http://192.168.1.105:8848/img/5',	//测试使用
 	// imgUrl:'http://192.168.1.105:8848/img'	,//运营使用
-	  
 }
